1c06a7357553b499ddb451c5545723ea06846b66
ZubairrRiaz <160521830+ZubairrRiaz@users.noreply.github.com>
2025-02-10 16:18:59 +0000

    Add files via upload

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -7,9 +7,7 @@ const Error404 = () => (
     <h1 className="t5xl font-bold mb-8">404 - Page Not Found</h1>
     <p>The page you are looking for does not exist or has been moved.</p>
     <Link href="/">
-      <a className="border py-10 border-black px-6 mt-14 rounded-md">
         Go Home
-      </a>
     </Link>
   </div>
 );
